cd5eaba05a306c44d657e9a6e7ddcca4a241b894
Ajal88 <amirabbasjl@yahoo.com>
2018-07-21 00:48:50 +0430

    9 compeleted

--- Codes/A3.R
+++ Codes/A3.R
@@ -1,39 +1,33 @@
-library(highcharter)
 library(ggplot2)
 
-company_year = read_csv("Downloads/R/DA_Project/Data/asn_c.csv") %>% as.data.frame(stringsAsFactors = F) %>% 
-  mutate(Total_occupants = ifelse(Total_occupants == 0 & Total_fatalities != 0, Total_fatalities, Total_occupants),
-         Total_survivors = abs(Total_occupants - Total_fatalities)) %>% 
-  mutate(is_army = str_detect(Operator, regex("Force|Navy",ignore_case = T))) %>% 
-  filter(is_army == FALSE) %>% 
-  select(C.n.msn,
-         year = Date,
-         Operator,
-         FirstFlight,
-         Total_occupants,
-         Total_fatalities,
-         TotalAirframeHrs,
-         Crew_occupants) %>% 
-  na.omit() %>% 
-  group_by(Operator, year) %>% 
-  summarise(count = n(), tot_fatal = sum(Total_fatalities), tot_occu = sum(Total_occupants)) %>% 
-  filter(tot_occu > 20, tot_fatal > 5) %>%
-  mutate(index = 1, index = cumsum(index), index = max(index)) %>% 
-  filter(index > 2) %>% 
-  select(-index) %>% 
-  mutate(death_rate = tot_fatal*100/tot_occu)
+airline_safety = read_csv("Downloads/R/DA_Project/Data/airline_safety.csv") %>% 
+  mutate(death_ask_85_99 = (fatalities_85_99/avail_seat_km_per_week)*10^6,
+         death_ask_00_14 = (fatalities_00_14/avail_seat_km_per_week)*10^6)
 
-company_year %>% 
-  hchart(type = "line",
-         hcaes(x = year, y = count, group = Operator)) %>%
-  hc_xAxis(title = list(text = "year")) %>%
-  hc_yAxis(title = list(text = "Accidents Count"),
-           max = 22,
-           tickInterval = 1,
-           min = 0,
-           plotLines = list(list(color = "#FF0000",
-                                 width = 2,
-                                 value = 11,
-                                 dashStyle = 'shortdash'))) %>% 
-  hc_title(text = "Accidents Count of an Airline in years",
-           style = list(fontWeight = "bold"))
+ggplot(airline_safety, aes(x = death_ask_85_99, y = death_ask_00_14)) +
+  geom_point() + 
+  geom_text(aes(label=ifelse(death_ask_00_14>0.4 | death_ask_85_99>0.4,as.character(airline),'')),
+            hjust=-0.1,
+            vjust=-0.1,
+            angle=10,
+            size=3) +
+  scale_x_continuous(name="Death in 1985 - 1999 ASK", limits=c(0, 1.2)) +
+  scale_y_continuous(name="Death in 2000 - 2014 ASK", limits=c(0, 1)) + 
+  geom_smooth(method = lm, se = FALSE) + 
+  ggtitle("Fatalities of Companies")
+  
+cor.test(airline_safety$fatalities_85_99, airline_safety$fatalities_00_14)
+
+ggplot(airline_safety, aes(x = incidents_85_99, y = incidents_00_14)) +
+  geom_point() + 
+  geom_text(aes(label=ifelse(incidents_00_14>10 | incidents_85_99>20,as.character(airline),'')),
+            hjust=0,
+            vjust=0,
+            angle=10,
+            size=3) +
+  scale_x_continuous(name="Incidents in 1985 - 1999 ASK") +
+  scale_y_continuous(name="Incidents in 2000 - 2014 ASK") + 
+  geom_smooth(method = lm, se = FALSE) + 
+  ggtitle("Incidents of Companies")
+
+cor.test(airline_safety$incidents_85_99, airline_safety$incidents_00_14)
---
+++ Codes/A9.R
@@ -0,0 +1,39 @@
+library(highcharter)
+library(ggplot2)
+
+company_year = read_csv("Data/asn_c.csv") %>% as.data.frame(stringsAsFactors = F) %>% 
+  mutate(Total_occupants = ifelse(Total_occupants == 0 & Total_fatalities != 0, Total_fatalities, Total_occupants),
+         Total_survivors = abs(Total_occupants - Total_fatalities)) %>% 
+  mutate(is_army = str_detect(Operator, regex("Force|Navy",ignore_case = T))) %>% 
+  filter(is_army == FALSE) %>% 
+  select(C.n.msn,
+         year = Date,
+         Operator,
+         FirstFlight,
+         Total_occupants,
+         Total_fatalities,
+         TotalAirframeHrs,
+         Crew_occupants) %>% 
+  na.omit() %>% 
+  group_by(Operator, year) %>% 
+  summarise(count = n(), tot_fatal = sum(Total_fatalities), tot_occu = sum(Total_occupants)) %>% 
+  filter(tot_occu > 20, tot_fatal > 5) %>% 
+  mutate(index = 1, index = cumsum(index), index = max(index)) %>% 
+  filter(index > 2) %>% 
+  select(-index) %>% 
+  mutate(death_rate = tot_fatal*100/tot_occu)
+
+company_year %>% 
+  hchart(type = "line",
+         hcaes(x = year, y = count, group = Operator)) %>%
+  hc_xAxis(title = list(text = "year")) %>%
+  hc_yAxis(title = list(text = "Accidents Count"),
+           max = 22,
+           tickInterval = 1,
+           min = 0,
+           plotLines = list(list(color = "#FF0000",
+                                 width = 2,
+                                 value = 11,
+                                 dashStyle = 'shortdash'))) %>% 
+  hc_title(text = "Accidents Count of an Airline in years",
+           style = list(fontWeight = "bold"))
