f05c6d63b28087b4d24a60c3d12870543801b082
mina <mina.m777@yahoo.com>
2018-07-21 13:44:44 +0430

    A10

---
+++ Codes/A10.R
@@ -0,0 +1,45 @@
+airfare <- read_csv("Data/Consumer_Airfare_Report__Table_5_-_Detailed_Fare_Information_For_Highest_and_Lowest_Fare_Markets_Under_750_Miles.csv")
+
+american_airlines <- airfare %>% select(car) %>% distinct(car) %>% arrange(car)
+pattern_name = c("Tropic", "Air Plus Comet", "American Airlines", "Alaska Airlines", "JetBlue", "Continental Air Lines",
+                 "Discovery Airways", "Delta Air Lines", "Frontier Airlines (FL)", "irTran Airways", "Allegiant Air",
+                 "America West Airlines", "", "", "Kiwi Regional Airlines", "", "Spirit Airlines", "Northwest",
+                 "", "", "Horizon Air", "", "", "", "Skydive Twin Cities", "", "United Airlines", "US Airways",
+                 "American Virginia", "Pacific Western Airlines", "", "Southwest Airlines", "", "", "Mesa Airlines",
+                 "Midwest", "Air Wisconsin")
+
+american_airlines <- data.frame(american_airlines, pattern_name)
+colnames(american_airlines ) <- c("car","Operator")
+
+airfare <- na.omit(airfare)
+airfare <- airfare %>% mutate(price = as.numeric(str_extract(mkt_fare,"\\d+")))
+sum_airfare <- airfare %>% group_by(car, Year) %>% summarise(price = mean(price))
+sum_airfare <- sum_airfare %>% inner_join(american_airlines, by = c("car")) %>% ungroup()
+
+american_crashes <- casn_topics %>% ungroup() %>% 
+  inner_join(american_airlines, by = c("Operator"))
+
+sum_american_crashes <- american_crashes %>% group_by(Operator, Date) %>% 
+  summarise(Total_occupants = sum(Total_occupants), 
+            Total_fatalities = sum(Total_fatalities),
+            Total_survivors = sum(Total_survivors),
+            occurance = n()) %>% 
+  mutate(Survival_rate = ifelse(Total_occupants < 1, 0, 100*Total_survivors/Total_occupants), 
+         Fatality_rate = ifelse(Total_occupants < 1, 0, 100*Total_fatalities/Total_occupants)) %>% 
+  ungroup()
+  
+Total_occupants_avg <- mean(sum_american_crashes$Total_occupants)
+sum_american_crashes <- sum_american_crashes %>% 
+  mutate(safety = ifelse(Total_occupants < 1, 0.1, Survival_rate - Total_occupants_avg/(occurance*Total_occupants)))
+
+crash_price <- sum_airfare %>% inner_join(sum_american_crashes, by = c("Operator", "Year" = "Date"))
+
+price_avg <- mean(crash_price$price)
+crash_price_high <- crash_price %>% filter(price > price_avg)
+crash_price_low <- crash_price %>% filter(price <= price_avg)
+
+kruskal.test(price ~ safety, data = crash_price)
+wilcox.test(crash_price_high$safety, crash_price_low$safety, alternative = "less", exact = FALSE, correct = FALSE)
+cor.test(formula = ~price + safety, data = crash_price)
+t.test(crash_price_high$safety, crash_price_low$safety, alternative = "less")
+
