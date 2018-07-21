2700e7783242c3a38ba0d820e875561b2f2112fb
mina <mina.m777@yahoo.com>
2018-07-21 20:38:19 +0430

    A10

--- Codes/A10.R
+++ Codes/A10.R
@@ -1,4 +1,4 @@
-airfare <- read_csv("../Data/Consumer_Airfare_Report__Table_5_-_Detailed_Fare_Information_For_Highest_and_Lowest_Fare_Markets_Under_750_Miles.csv")
+airfare <- read_csv("Data/Consumer_Airfare_Report__Table_5_-_Detailed_Fare_Information_For_Highest_and_Lowest_Fare_Markets_Under_750_Miles.csv")
 
 american_airlines <- airfare %>% select(car) %>% distinct(car) %>% arrange(car)
 pattern_name = c("Tropic", "Air Plus Comet", "American Airlines", "Alaska Airlines", "JetBlue", "Continental Air Lines",
@@ -14,10 +14,11 @@ colnames(american_airlines ) <- c("car","Operator")
 airfare <- na.omit(airfare)
 airfare <- airfare %>% mutate(price = as.numeric(str_extract(mkt_fare,"\\d+")))
 sum_airfare <- airfare %>% group_by(car, Year) %>% summarise(price = mean(price))
-sum_airfare <- sum_airfare %>% inner_join(american_airlines, by = c("car")) %>% ungroup()
+sum_airfare <- sum_airfare %>% inner_join(american_airlines, by = c("car")) %>% ungroup() %>% 
+  group_by(Operator) %>% summarise(price = mean(price)) %>% 
+  mutate(need = 0)
 
-american_crashes <- casn_topics %>% ungroup() %>% 
-  inner_join(american_airlines, by = c("Operator"))
+# american_crashes <- casn_topics %>% ungroup() %>% inner_join(american_airlines, by = c("Operator"))
 
 airlines_name = as.data.frame(c("Virgin Atlantic",
                   "Southwest Airlines",
@@ -28,32 +29,13 @@ airlines_name = as.data.frame(c("Virgin Atlantic",
                   "US Airways / America West*"))
 colnames(airlines_name) =  c("airline")
 
-airline_safety_total = read_csv("../Data/airline_safety.csv") %>% 
+airline_safety_total = read_csv("Data/airline_safety.csv") %>% 
   mutate(total_fatal_accidents = (fatal_accidents_85_99 + fatal_accidents_00_14),
          total_incidents = (incidents_85_99 + incidents_00_14)) %>% 
   select(airline, avail_seat_km_per_week, total_fatal_accidents, total_incidents) %>% 
   mutate(score = 20 - (9*total_fatal_accidents + total_incidents)*10^8/avail_seat_km_per_week)
 
 airline_safety_total = merge(airline_safety_total, airlines_name, sort = FALSE)
-
-sum_american_crashes <- american_crashes %>% group_by(Operator, Date) %>% 
-  summarise(Total_occupants = sum(Total_occupants), 
-            Total_fatalities = sum(Total_fatalities),
-            Total_survivors = sum(Total_survivors),
-            occurance = n()) %>% 
-  mutate(Survival_rate = ifelse(Total_occupants < 1, 0, 100*Total_survivors/Total_occupants), 
-         Fatality_rate = ifelse(Total_occupants < 1, 0, 100*Total_fatalities/Total_occupants)) %>% 
-  ungroup()
-  
-Total_occupants_avg <- mean(sum_american_crashes$Total_occupants)
-sum_american_crashes <- sum_american_crashes %>% 
-  mutate(safety = ifelse(Total_occupants < 1, 0.1, Survival_rate - Total_occupants_avg/(occurance*Total_occupants)))
-
-crash_price <- sum_airfare %>% inner_join(sum_american_crashes, by = c("Operator", "Year" = "Date"))
-
-price_avg <- mean(crash_price$price)
-crash_price_high <- crash_price %>% filter(price > price_avg)
-crash_price_low <- crash_price %>% filter(price <= price_avg)
 
 kruskal.test(price ~ safety, data = crash_price)
 wilcox.test(crash_price_high$safety, crash_price_low$safety, alternative = "less", exact = FALSE, correct = FALSE)
--- Codes/A14.R
+++ Codes/A14.R
@@ -8,4 +8,38 @@ colnames(rest_Iranian_operators) = c("Operator")
 iran_casn <- iran_casn %>% 
   bind_rows(casn %>% inner_join(rest_Iranian_operators, by = c("Operator")))
 
+iran_casn <- iran_casn %>% 
+  mutate(kind = ifelse(Total_fatalities > 0, "Crash", "Incident"))
+
+# a: sanctions effect
+iran_sum <- iran_casn %>% group_by(Date, kind) %>% 
+  summarise(count = n(), fatalities = sum(Total_fatalities))
+
+iran_sum <- iran_casn %>% ungroup() %>% group_by(Date) %>% 
+  summarise(count = n(), fatalities = sum(Total_fatalities)) %>% 
+  mutate(kind = "All") %>% bind_rows(iran_sum)
+
+iran_sum  %>% 
+  hchart(type = "column", hcaes(x = Date, y = count, group = kind)) %>% 
+  hc_yAxis(title = list(text = "Count")) %>% 
+  hc_xAxis(title = list(text = "Year"), 
+           plotLines = list(list(color = "#FF0000", width = 2, value = 1978, dashStyle = 'shortdash', 
+                                 label = list(text = "Iran Revolution - Sanctions")))) %>% 
+  hc_title(text = "Iran Safety Occurance", style = list(fontWeight = "bold")) %>% 
+  hc_add_theme(hc_theme_flat())
+
+iran_before <- iran_sum %>% filter(Date < 1978 & kind == "All")
+iran_after <- iran_sum %>% filter(Date >= 1978 & kind == "All")
+t.test(iran_before$count, iran_after$count, alternative = "less")
+
+# 2
+
+# 6
+
+# 7
+
+# 8
+
+
+
 
