61e8d34711292d305558445081edfe23a3764108
Ajal88 <amirabbasjl@yahoo.com>
2018-07-21 15:35:31 +0430

    checking error

--- Codes/A10.R
+++ Codes/A10.R
@@ -1,4 +1,4 @@
-airfare <- read_csv("Data/Consumer_Airfare_Report__Table_5_-_Detailed_Fare_Information_For_Highest_and_Lowest_Fare_Markets_Under_750_Miles.csv")
+airfare <- read_csv("../Data/Consumer_Airfare_Report__Table_5_-_Detailed_Fare_Information_For_Highest_and_Lowest_Fare_Markets_Under_750_Miles.csv")
 
 american_airlines <- airfare %>% select(car) %>% distinct(car) %>% arrange(car)
 pattern_name = c("Tropic", "Air Plus Comet", "American Airlines", "Alaska Airlines", "JetBlue", "Continental Air Lines",
@@ -19,11 +19,22 @@ sum_airfare <- sum_airfare %>% inner_join(american_airlines, by = c("car")) %>% 
 american_crashes <- casn_topics %>% ungroup() %>% 
   inner_join(american_airlines, by = c("Operator"))
 
+airlines_name = as.data.frame(c("Virgin Atlantic",
+                  "Southwest Airlines",
+                  "United / Continental*",
+                  "Alaska Airlines*",
+                  "American*",
+                  "Delta / Northwest*",
+                  "US Airways / America West*"))
+colnames(airlines_name) =  c("airline")
+
 airline_safety_total = read_csv("../Data/airline_safety.csv") %>% 
   mutate(total_fatal_accidents = (fatal_accidents_85_99 + fatal_accidents_00_14),
          total_incidents = (incidents_85_99 + incidents_00_14)) %>% 
   select(airline, avail_seat_km_per_week, total_fatal_accidents, total_incidents) %>% 
   mutate(score = 20 - (9*total_fatal_accidents + total_incidents)*10^8/avail_seat_km_per_week)
+
+airline_safety_total = merge(airline_safety_total, airlines_name, sort = FALSE)
 
 sum_american_crashes <- american_crashes %>% group_by(Operator, Date) %>% 
   summarise(Total_occupants = sum(Total_occupants), 
